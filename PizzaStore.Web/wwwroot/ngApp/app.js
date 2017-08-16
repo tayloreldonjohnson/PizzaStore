681f429039449e039eabf4c558b0fa5409563dcd
Carlos Montiel <carlosmontiel2@gmail.com>
2017-08-16 11:32:02 -0700

    angular

--- PizzaStore.Web/wwwroot/ngApp/app.js
+++ PizzaStore.Web/wwwroot/ngApp/app.js
@@ -1,1 +1,12 @@
-﻿
+﻿var myApp = angular.module("myApp", ["ui.router"]);
+myApp.controller("MainController", MainController);
+myApp.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider.state('main', {
+        url: '/',
+        templateUrl: '/NgApp/views/main.html',
+        controller: MainController,
+        controllerAs: 'controller'
+    });
+    $urlRouterProvider.otherwise('/notFound');
+    $locationProvider.html5Mode(true);
+}); 
